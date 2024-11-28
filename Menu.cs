d05f03434f15b1d007bee5ac68541bef0ca0f27d
NamiAndisheh <Nami.Andisheh1@gmail.com>
2024-11-28 14:41:15 +0100

    added first template files

---
+++ Menu.cs
@@ -0,0 +1,88 @@
+namespace MenuWithDatabase;
+
+public class Menu
+{
+    Actions _actions;
+    public Menu(Actions actions)
+    {
+        // constructorn tar emot actions 
+        _actions = actions;
+        // och startar menyn
+        PrintMenu();
+    }
+
+    private void PrintMenu()
+    {
+        // skriver ut menyn i konsolen
+        Console.WriteLine("Choose option");
+        Console.WriteLine("1. List all");
+        Console.WriteLine("2. Show one");
+        Console.WriteLine("3. Add one");
+        Console.WriteLine("4. Update one");
+        Console.WriteLine("5. Delete one");
+        Console.WriteLine("9. Quit");
+        // lyssnar på användaren
+        AskUser();
+    }
+
+    private async void AskUser()
+    {
+        // tar emot vad användaren skriver
+        var response = Console.ReadLine();
+        if (response is not null)
+        {
+            string? id; // define for multiple use below
+
+            // kör olika actions beroende på vad användaren skrivit
+            switch (response)
+            {
+                case("1"):
+                    Console.WriteLine("Listing all");
+                    _actions.ListAll();
+                    break;
+                case("2"):
+                    Console.WriteLine("Enter id to show details about one");
+                    id = Console.ReadLine();
+                    if (id is not null)
+                    { 
+                        _actions.ShowOne(id);
+                    }
+                    break;
+                case("3"):
+                    Console.WriteLine("Enter name (required)");
+                    var name = Console.ReadLine(); // required
+                    Console.WriteLine("Enter slogan");
+                    var slogan = Console.ReadLine(); // not required
+                    if (name is not null)
+                    {
+                        _actions.AddOne(name, slogan);
+                    }
+                    break;
+                case("4"):
+                    Console.WriteLine("Enter id to update one");
+                    id = Console.ReadLine();
+                    if (id is not null)
+                    { 
+                        _actions.UpdateOne(id);
+                    }
+                    break;
+                case("5"):
+                    Console.WriteLine("Enter id to delete one");
+                    id = Console.ReadLine();
+                    if (id is not null)
+                    { 
+                        _actions.DeleteOne(id);
+                    }
+                    break;
+                case("9"):
+                    Console.WriteLine("Quitting");
+                    Environment.Exit(0);
+                    break;
+            }
+
+            PrintMenu();
+        }
+
+    }
+
+}
